5f3f2df4eb520a698f5a0b8f4b0e1f8f570aed7a
Jessica McCollum <info.jessicamccollum@gmail.com>
2022-07-11 14:45:58 -1000

    Quarter of the year completed

---
+++ 8kyu/quarterOfTheYear/js/main.js
@@ -0,0 +1,26 @@
+const quarterOf = (month) => {
+    switch (month) {
+        case 1:
+        case 2:
+        case 3:
+          return 1
+        case 4:
+        case 5:
+        case 6:
+          return 2
+        case 7:
+        case 8:
+        case 9:
+          return 3
+        case 10:
+        case 11:
+        case 12:
+          return 4
+    }
+}
+
+console.log(quarterOf(3))
+console.log(quarterOf(10))
+console.log(quarterOf(7))
+console.log(quarterOf(5))
+
